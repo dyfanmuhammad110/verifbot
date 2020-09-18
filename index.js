e9a27bb5883876d933a3faeebb4eb2237185668d
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2020-09-18 09:40:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://dyfan-bot.glitch.me"]
+const urls = ["https://dyfan145-dyfan-code-1.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
